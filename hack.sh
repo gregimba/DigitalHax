8960c218c52c501889b0867d2f7329c0e2d80874
Jeffrey Paul <sneak@datavibe.net>
2013-12-31 03:20:16 +0100

    fixed it to not overwrite data onto the disk it's trying to recover from

--- hack.sh
+++ hack.sh
@@ -1,7 +1,5 @@
 #Assuming root access and ubuntu 12.04
 echo "Ip address"
 read ip
-ssh root@$ip 'bash -s' < find.sh
-#Then just run wget 0.0.0.0/out.txt.gz
-echo "Fetching file"
-wget $ip/out.txt.gz
+scp ./find.sh root@$ip:
+ssh root@$ip 'chmod +x ./find.sh ; ./find.sh' | pv > out.txt.gz
